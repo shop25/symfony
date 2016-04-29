2a3c23c23661c80f4896731146f78582cca65caf
Matt Lehner <matt.lehner@coachmeplus.com>
2016-04-29 11:21:37 -0400

    include arguments in annotated Doctrine_Record methods

--- lib/plugins/sfDoctrinePlugin/lib/task/sfDoctrineBuildModelTask.class.php
+++ lib/plugins/sfDoctrinePlugin/lib/task/sfDoctrineBuildModelTask.class.php
@@ -95,8 +95,8 @@ EOF;
           $camelized = sfInflector::camelize($name);
           $collection = 'Doctrine_Collection' == $type;
 
-          $getters[] = sprintf('@method %-'.$typePad.'s %s%-'.($namePad + 2).'s Returns the current record\'s "%s" %s', $type, 'get', $camelized.'()', $name, $collection ? 'collection' : 'value');
-          $setters[] = sprintf('@method %-'.$typePad.'s %s%-'.($namePad + 2).'s Sets the current record\'s "%s" %s', $model, 'set', $camelized.'()', $name, $collection ? 'collection' : 'value');
+          $getters[] = sprintf('@method %-'.$typePad.'s %s%-'.($namePad + 2).'s Returns the current record\'s "%s" %s', $type, 'get', $camelized.'($load = true)', $name, $collection ? 'collection' : 'value');
+          $setters[] = sprintf('@method %-'.$typePad.'s %s%-'.($namePad + 2).'s Sets the current record\'s "%s" %s', $model, 'set', $camelized.'($value, $load = true)', $name, $collection ? 'collection' : 'value');
         }
 
         // use the last match as a search string
